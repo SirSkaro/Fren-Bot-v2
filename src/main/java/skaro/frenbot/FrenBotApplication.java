ab70a7ac03b84bf92cf949e08a6bd19b30429ce9
unknown <bencbchurchill@gmail.com>
2019-06-29 18:21:11 -0700

    actual first commit... lol

---
+++ src/main/java/skaro/frenbot/FrenBotApplication.java
@@ -0,0 +1,13 @@
+package skaro.frenbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skaro/frenbot/FrenBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skaro.frenbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrenBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
